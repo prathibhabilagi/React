443dc0a32b98170af76d46351cc2fdb61fe18e05
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-15 22:16:26 +0530

    List Elements

---
+++ JSX Components/src/components/list.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>RM</li>
+      <li>Jin</li>
+      <li>Suga</li>
+      <li>JHope</li>
+      <li>Jimin</li>
+      <li>V</li>
+      <li>JK</li>
+    </ul>
+  );
+}
+
+export default List;
