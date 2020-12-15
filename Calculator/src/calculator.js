0c64a5905b04277f7cfb1b3fcbbdd79bb813d077
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-15 22:23:29 +0530

    JS File ( functions => add, subtract, multiply and divide)

---
+++ Calculator/src/calculator.js
@@ -0,0 +1,17 @@
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function sub(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+export { add, sub, multiply, divide };
