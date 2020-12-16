fd1c244faed4503d6f5082704a6a9bf313912538
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-16 18:17:58 +0530

    JSX file import(Header, Note and Footer)

---
+++ Keeper-App/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Notes from "./Note.jsx";
+
+function App(){
+    return(
+        <div>
+            <Heading />
+            <Notes />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
