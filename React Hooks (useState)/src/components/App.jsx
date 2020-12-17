2ae89b98a40e6006676fb9326b4b3dc64c5602fb
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-17 18:09:53 +0530

    Return CurrentTime (onClick) using useState

---
+++ React Hooks (useState)/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function App() {
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString());
+  }
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
+    </div>
+  );
+}
+export default App;
