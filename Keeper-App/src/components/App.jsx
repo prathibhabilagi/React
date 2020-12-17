751d59631910b7e1a4b86ccbea88cb5421181e6b
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-17 17:53:11 +0530

    Updating and import (notesKeeper contains Array Elements)

--- Keeper-App/src/components/App.jsx
+++ Keeper-App/src/components/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Heading from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Notes from "./Note.jsx";
+import notesKeeper from "../notesKeeper";
 
 function App(){
     return(
         <div>
             <Heading />
-            <Notes />
+            {notesKeeper.map((noteItem) => <Notes
+                key = {noteItem.id}
+                title = {noteItem.title}
+                content = {noteItem.content}
+            />
+            )}
             <Footer />
         </div>
     );
