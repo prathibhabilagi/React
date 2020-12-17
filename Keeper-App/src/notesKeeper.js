b71b401fb7a2c455ddbf35b5d575d445cdcc1e60
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-17 17:55:59 +0530

    Js File (Array containing unique ID)

---
+++ Keeper-App/src/notesKeeper.js
@@ -0,0 +1,29 @@
+const notesKeeper =[
+    {
+        id: 1,
+        title : "Kim Nam Joon",
+        content: "RM, better known by his stage name RM, is a South Korean rapper, songwriter, and record producer. He is the leader of the South Korean boy group BTS."
+    },
+    {
+        id: 2,
+        title: "Kim Seok Jin",
+        content: "Jin, also known by his stage name Jin, is a South Korean singer, songwriter, and member of the South Korean boy band BTS since June 2013. "
+    },
+    {
+        id: 3,
+        title: "Min Yoongi",
+        content: "Suga, better known by his stage names Suga and Agust D, is a South Korean rapper, songwriter, and record producer."
+    },
+    {
+        id: 4,
+        title: "Jung Ho-seok",
+        content: "JHope, better known by his stage name J-Hope, is a South Korean rapper, songwriter, dancer, and record producer. In 2013,"
+    },
+    {
+        id: 5,
+        title: "Kim Seok Jin",
+        content: "Jin, also known by his stage name Jin, is a South Korean singer, songwriter, and member of the South Korean boy band BTS since June 2013. "
+    }
+]
+
+export default notesKeeper;
