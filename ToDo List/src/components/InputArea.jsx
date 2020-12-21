3b93a34f797224d9374ff96eda033396c5c25455
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2020-12-21 19:18:05 +0530

    Input todoItem and Handle change

---
+++ ToDo List/src/components/InputArea.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    setInputText(event.target.value);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        onClick={() => {
+          props.onAdd(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+export default InputArea;
