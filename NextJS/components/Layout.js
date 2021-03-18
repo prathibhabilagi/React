0f3d48348af8a3875ec702f00a12ba2dddf14a33
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-03-18 18:41:25 +0530

    Creating Layout to render header, footer and child properties

--- NextJS/components/Layout.js
+++ NextJS/components/Layout.js
@@ -2,12 +2,15 @@ import Nav from "./Nav";
 
 const Layout = ({ mainTitle, footer, children }) => (
     <div>
+    
         <Nav />
+    
         <h1>{mainTitle}</h1>
         <hr />
         {children}
         <hr />
         <h4>{footer}</h4>
+    
     </div>
 );
 
