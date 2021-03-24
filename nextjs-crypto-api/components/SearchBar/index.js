49096a0d003a90a6d3a392c32c841b1745a3a605
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-03-24 14:37:23 +0530

    Adding useState

--- nextjs-crypto-api/components/SearchBar/index.js
+++ nextjs-crypto-api/components/SearchBar/index.js
@@ -1,11 +1,50 @@
 import styles from './Search.module.css';
+import Link from 'next/link';
+import {useState} from 'react';
 
 const SearchBar = ({ ...rest }) => {
-  return (
+  const [query, setQuery] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  function handleParam(e){
+    setQuery(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  const search = () => {
+    setLoading(true)
+    fetch(`https://api.coingecko.com/api/v3/coins/${query}`)
+    .then(response => response.json())
+    .then(json => {console.log(json);
+    setLoading(false)}
+    )
+  }
+
+  return ( 
+    <Link href="/coin/[id]" as={`/coin/${query}`}>
+   
     <div className={styles.coin_search}>
-      <input className={styles.coin_input} {...rest} />
+      <form onSubmit={onSubmit} className={styles.form}>  
+        <input 
+          className={styles.coin_input} 
+          onChange={handleParam}
+          placeholder='Search'
+          value={query}
+          {...rest}
+        /> 
+        {!isLoading && <button className={styles.button} onClick={search} >Search</button>}
+        {isLoading && <button className={styles.button} disabled >Loading</button>}
+      </form>
     </div>
+   </Link>
   );
 };
+
+export default SearchBar;
+
+
 
 export default SearchBar;
