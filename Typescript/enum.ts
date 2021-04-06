d49a3f76361b18fe80b9daf1248bc9e2c5e88dda
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-04-06 19:21:12 +0530

    Group of named constants (Months of year)

---
+++ Typescript/enum.ts
@@ -0,0 +1,21 @@
+enum MonthsOfYear {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+}
+
+let month: MonthsOfYear;
+month = MonthsOfYear.Jan;
+
+if(month === MonthsOfYear.Jan){
+    console.log("1st month of the year");
+}
