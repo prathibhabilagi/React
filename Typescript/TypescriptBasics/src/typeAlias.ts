59294f3ceeba621b85539b188fe2bfc098300b81
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-04-28 01:37:09 +0530

    Creating type alias of Student

---
+++ Typescript/TypescriptBasics/src/typeAlias.ts
@@ -0,0 +1,17 @@
+type KG = number;
+
+type Student = {
+    name: string,
+    ID: number,
+    weight: KG
+}
+
+let student1: Student = {
+    name: "Tae",
+    ID: 7,
+    weight: 56
+}
+
+console.log(student1);
+
+
