2a7d02af0615feea7ac88407ddb394c078057e93
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-04-29 23:30:01 +0530

    Make API calls and making use of useState and useEffect

---
+++ breaking-bad-api/src/App.js
@@ -0,0 +1,29 @@
+import React, {  useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import './App.css';
+
+const App = () =>  {
+  const[items, setItems] = useState([]);
+  const[isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async() => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      //console.log(result.data);
+      setItems(result.data);
+      setLoading(false);
+    }
+    fetchItems()
+  }, [])
+
+  return (
+    <div className="App">
+      <Header/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
+    </div>
+  );
+}
+
+export default App;
